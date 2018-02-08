c15f1082af8b7ce8a307293bf212a71c75ecd3e1
alexkotov13 <alex.kotov.13.10@gmail.com>
2018-02-08 16:42:39 +0200

    Add files via upload

---
+++ TestTask_bMuse/TestTask_bMuse/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Data.Entity;
+using Newtonsoft.Json;
+
+namespace TestTask_bMuse
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Context db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new Context();
+        }
+
+        public void OutputTable()
+        {
+            try
+            {
+                string jsonWeather = System.IO.File.ReadAllText(@"C:\Users\Алексей\Desktop\TestTask_bMuse\TestTask_bMuse\jsonWeather.txt", Encoding.Default);
+
+                var objectWeather = JsonConvert.DeserializeObject<Weather>(jsonWeather);
+                Database.SetInitializer<Context>(null);
+                // add to the database
+                db.WeatherByMonth.Add(objectWeather);
+                // get objects from the bd and output to the table
+                DataGridWeather.ItemsSource = db.WeatherByMonth.Local.ToBindingList();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void ButtonGetInfoClick(object sender, RoutedEventArgs e)
+        {
+            OutputTable();
+        }
+
+        private void RowClicked(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DataGridWeather.ItemsSource = db.WeatherByDate.Local.ToBindingList();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void MenuExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void MenuHelpClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To open the collection, double-click on the cell");
+        }
+    }
+}
---
+++ TestTask_bMuse/TestTask_bMuse/Weather.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+
+namespace TestTask_bMuse
+{
+    [Table("Weather")]
+    public class Weather
+    {
+        [Key]
+        [JsonProperty("month")]
+        public string Month { get; set; }
+
+        [JsonProperty("data")]
+        public List<WeatherDataList> Data { get; set; }
+
+    }
+
+    [Table("WeatherDataList")]
+    public class WeatherDataList
+    {
+        [Key]
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("max_temp")]
+        public int MaxTemp { get; set; }
+
+        [JsonProperty("min_temp")]
+        public int MinTemp { get; set; }
+    }
+}
+
+
+
